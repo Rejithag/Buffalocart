69b7a718292ab9efcde85867a886ed016722e20a
Rejithag <88918498+Rejithag@users.noreply.github.com>
2021-10-15 08:11:45 +0530

    Delete java/com/buffalocart directory

--- java/com/buffalocart/constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.buffalocart.constants;
-
-public class Constants {
-
-	public static final String CONFIGFILE = "/src/main/resources/config.properties";
-	public static final String EXCEL_FILE = "\\src\\main\\resources\\Buffalocart_TestData.xlsx";
-
-}
